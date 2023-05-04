963a44c2f1dc764ac95e848477c838e0b272029b
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-04 16:48:07 -0700

    Filter by greased

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import HogControls from "./HogControls"
 import HogList from "./HogList"
@@ -6,11 +6,20 @@ import HogList from "./HogList"
 import hogs from "../porkers_data";
 
 function App() {
+
+	const [isGreased, setIsGreased] = useState(false)
+
+	const changeGreased = () => {
+		setIsGreased(currentValue => !currentValue)
+	}
+
+	const filteredHogs = hogs.filter(hog => isGreased === false || hog.greased === isGreased)
+
 	return (
 		<div className="App">
 			<Nav />
-			<HogControls /> 
-			<HogList hogs={hogs} /> 
+			<HogControls changeGreased={changeGreased} /> 
+			<HogList hogs={filteredHogs} /> 
 		</div>
 	);
 }
--- src/components/HogControls.js
+++ src/components/HogControls.js
@@ -1,6 +1,17 @@
-const HogControls = () => {
+const HogControls = ({ changeGreased }) => {
     return (
-        <h2>Controls go here!</h2>
+        <div className="filterWrapper">
+            <h3>Filter hogs</h3>
+          
+            <input onChange={changeGreased} type='checkbox' name='greased' />
+            <label htmlFor='greased'>Greased?</label>
+
+            <select defaultValue='Sort by ...'>
+                <option disabled>Sort by ...</option>
+                <option>name</option>
+                <option>weight</option>
+            </select>
+        </div>        
     )
 }
 
